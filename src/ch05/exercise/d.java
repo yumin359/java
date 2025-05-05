2a376db1309345c7090f0569482ec32698379cde
yumin359 <yumin359@gmail.com>
2025-05-05 16:09:59 +0900

    home

---
+++ src/ch05/exercise/d.java
@@ -0,0 +1,8 @@
+package ch05.exercise;
+
+public class d {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+
+    }
+}
---
+++ src/ch05/exercise/d1.java
@@ -0,0 +1,7 @@
+package ch05.exercise;
+
+public class d1 {
+    public static void main(String[] args) {
+
+    }
+}
