ec65b2067d1ef0b55d9da8f1d225ca13e171d44d
yumin359 <yumin359@gmail.com>
2025-05-27 11:16:37 +0900

    0526 18장 filter stream중 printstream과 printwriter

---
+++ src/ch18/lecture/p05filter/App07.java
@@ -0,0 +1,31 @@
+package ch18.lecture.p05filter;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+public class App07 {
+    public static void main(String[] args) throws IOException {
+        String file = "C:/Temp/filter07.txt";
+        try (OutputStream os = new FileOutputStream(file)) {
+            os.write('a');
+            os.write('b');
+        }
+
+        // OutputStreamWriter : output stream을 writer로 변환
+
+        // PrintStream : output stream을 사용해 프린트하듯
+        // 출력하는 메소드가 있음
+
+        OutputStream os = new FileOutputStream(file);
+        PrintStream ps = new PrintStream(os);
+        try (os; ps) {
+            ps.println("ab");
+            ps.println("xy");
+            ps.println("한글");
+            ps.println("😁😎");
+        }
+
+    }
+}
---
+++ src/ch18/lecture/p05filter/App08.java
@@ -0,0 +1,28 @@
+package ch18.lecture.p05filter;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+
+public class App08 {
+    public static void main(String[] args) throws IOException {
+        String file = "C:/Temp/filter08.txt";
+        try (Writer writer = new FileWriter(file)) {
+            writer.write('a');
+            writer.write('b');
+            writer.write('한');
+        }
+
+        // PrintWriter : Writer를 감싸는 filter stream
+        // print 출력하듯 기능하는 print .. 메소드들이 있음
+        Writer writer = new FileWriter(file);
+        PrintWriter pw = new PrintWriter(writer);
+
+        try (writer; pw) {
+            pw.println("abxy");
+            pw.println("한글 가능");
+            pw.println("🤟👏✊");
+        }
+    }
+}
